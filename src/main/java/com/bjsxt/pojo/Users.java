2f337015dd4518ef061c569b27ee185298e701c3
Administrator <abc@163.com>
2019-03-20 00:40:40 +0800

    11

---
+++ src/main/java/com/bjsxt/pojo/Users.java
@@ -0,0 +1,27 @@
+package com.bjsxt.pojo;
+
+public class Users {
+	private int userid;
+	private String username;
+	private int userage;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getUserage() {
+		return userage;
+	}
+	public void setUserage(int userage) {
+		this.userage = userage;
+	}
+	
+	
+}
